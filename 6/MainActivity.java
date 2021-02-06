f21bf6cfd26aee89a20b9f3b73f8c9f777fc8a1a
SonyNareddy <57749150+SonyNareddy@users.noreply.github.com>
2021-02-06 21:16:18 +0530

    Update MainActivity.java

--- 6/MainActivity.java
+++ 6/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity  {
             public void onClick(View v) {
                 try {
                     FileOutputStream fOut=openFileOutput(file, MODE_PRIVATE);
-                    data = "Ramya \n17wh1a1243 Ramya\nRamya 1243\n";
+                    data = "Sony \n17wh1a1210 Sony\nSony 1210\n";
                     fOut.write(data.getBytes());
 
 
